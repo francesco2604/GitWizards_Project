a0f5552112a2b7abc412e5f121d47ed2a88714f8
Dan967 <daniele.francescatti@studenti.unitn.it>
2018-10-26 11:26:48 +0200

    modificato app.js come da esempio

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+ 
+ 
+app.get('/', (req, res) => res.send('Hello World!'))
+ 
+app.listen(PORT, () => console.log('Example app listening on port '+ PORT))
 
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
- res.statusCode = 200;
- res.setHeader('Content-Type', 'text/plain');
- res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
- console.log(`Server running at http://${hostname}:${port}/`);
-});
