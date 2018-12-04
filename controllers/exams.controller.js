22d717ac6358fc676e4bcfb9e41641b13229337c
francesco <francesco.gava@studenti.unitn.it>
2018-12-04 19:21:27 +0100

    #18 added method post into controller

--- controllers/exams.controller.js
+++ controllers/exams.controller.js
@@ -8,4 +8,18 @@ function getExamsList()
 {
   return examRepositories.getList();
 }
+function postExams(exam_post, identity)
+{
+  if((exam_post['teacher'])['id']!=identity.user_id || identity.user_role!= 2)
+  return 'No permission'
+  else {
+        if(exam_post.description === undefined || exam_post.numerotasks=== undefined)
+        return 'Error'
+        idSequence++;
+        exam_post.id = idSequence
+        examList.push(exam_post)
+        examRepositories.updateList(examList)
+        return exam_post
+  }
+}
 module.exports = {getExamsList}
