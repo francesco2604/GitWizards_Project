ec2fe81269aa0abdf3ec91634abcffbeccdcf202
francesco <francesco.gava@studenti.unitn.it>
2018-12-04 18:57:06 +0100

    method getlist for teacher equals getList for a student in routers

--- routers/exams.router.js
+++ routers/exams.router.js
@@ -3,7 +3,7 @@ var functions = require('../controllers/exams.controller.js');
 var result_response               = require('../models/error.model')
 var router = express.Router();
 
-// METHOD GET LIST
+// METHOD GET LIST FOR TEACHER AND STUDENT
 router.get('/', async (req,res) => {
 	var examList = functions.getExamsList();
 	res.status(200).json(examList);
