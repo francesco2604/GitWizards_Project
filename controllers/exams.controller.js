faa98c6a977bb819498da108ac95f920d8291072
francesco <francesco.gava@studenti.unitn.it>
2018-12-04 11:49:30 +0100

    #26 added controller for getlist student into controllers

---
+++ controllers/exams.controller.js
@@ -0,0 +1,12 @@
+var ExamRepository = require('../repositories/exam.repositories.js');
+var examRepositories=(new ExamRepository())
+var examList= examRepositories.getList();
+var propertiesForUpdate = ['description', 'numerotasks'];
+var idSequence = 1000;
+
+
+// GET EXAMS LIST
+function getExamsList()
+{
+  return examRepositories.getList();
+}
