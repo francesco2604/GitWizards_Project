41e96eab69e4e8444c54b4c9aa1e46c7450602a5
francesco <francesco.gava@studenti.unitn.it>
2018-12-04 19:24:24 +0100

    #18 added method post into router exam

--- routers/exams.router.js
+++ routers/exams.router.js
@@ -8,5 +8,19 @@ router.get('/', async (req,res) => {
 	var examList = functions.getExamsList();
 	res.status(200).json(examList);
 });
+// METHOD POST FOR TEACHER
+router.post('/', (req, res) => {
+	var propertiesPost = req.body;
+	var identity= req.headers;
+	var risposta=functions.postExams(propertiesPost,identity);
+	if(risposta==='Error')
+		sendErrorResponse(res, result_response.ERROR_CODE.NOT_FOUND , "Non trovato");
+	else{
+			if(risposta==='No permission')
+				sendErrorResponse(res, result_response.ERROR_CODE.FORBIDDEN , "Non permesso");
+			else
+				res.status(200).location('/v1/users/${risposta.id}').json(risposta);
+		}
+});
 
 module.exports = router
