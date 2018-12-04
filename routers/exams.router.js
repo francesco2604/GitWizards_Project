2507509341675565f6a47c083ab5b14664eda4de
francesco <francesco.gava@studenti.unitn.it>
2018-12-04 15:28:39 +0100

    #26 add router exams get

---
+++ routers/exams.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+var functions = require('../controllers/exams.controller.js');
+var result_response = require('../models/error.model')
+var router = express.Router();
+
+// METHOD GET
+router.get('/', async (req,res) => {
+	var examList = functions.getExamsList();
+	res.status(200).json(examList);
+});
+module.exports = router
