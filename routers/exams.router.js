b0fb8df00c845f47d1341589abd4adb1f57ae04b
francesco <francesco.gava@studenti.unitn.it>
2018-12-04 19:03:39 +0100

    5 updated file roter

--- routers/exams.router.js
+++ routers/exams.router.js
@@ -3,7 +3,7 @@ var functions = require('../controllers/exams.controller.js');
 var result_response               = require('../models/error.model')
 var router = express.Router();
 
-// METHOD GET LIST FOR TEACHER AND STUDENT
+// METHOD GET LIST FOR  STUDENT TEACHER AND
 router.get('/', async (req,res) => {
 	var examList = functions.getExamsList();
 	res.status(200).json(examList);
