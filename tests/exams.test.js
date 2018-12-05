2e1fbba0ff4182260dd318538da99180982478b2
francesco <francesco.gava@studenti.unitn.it>
2018-12-05 17:57:40 +0100

    #5 added test for getlist teacher

--- tests/exams.test.js
+++ tests/exams.test.js
@@ -4,11 +4,16 @@ var Exam = require('../models/exams.object.js');
 var router = require('../routers/exams.router.js');
 var functions = require('../controllers/exams.controller.js');
 var ExamRepository = require('../repositories/exam.repositories.js');
-const tasks = [{id: 123456,numeroDomanda: 2,question: 'diametro della Terra?',type: 1,answers: ['9.742 km','19.742 km','12.742 km'],correctAnswer: '3',studentAnswer: '1'},
-                {id: 85884,numeroDomanda: 2,question: 'diametro della Terra?',type: 1,answers: ['9.742 km','19.742 km','12.742 km'],correctAnswer: '3',studentAnswer: '1'}];
-var teacher ={id:32,firstname: 'mario',lastname: 'rossi',email: 'prova@gmail.com',type: 1,identification_number:123456 };
-var students = [{id:89,firstname: 'francesco ',lastname: 'persi',email: 'prova@gmail.com',type: 0,identification_number:1875698 },
-                {id:85,firstname: 'carlo',lastname: 'conti',email: 'prova@gmail.com',type: 0,identification_number:187525 }];
+const tasks = [{id: 123456,question: 'diametro della Terra?',answers: ['9.742 km','19.742 km','12.742 km'],correct_answer: '3',student_answer: '1'},
+                {id: 85884,question: 'diametro della Terra?',answers: ['9.742 km','19.742 km','12.742 km'],correct_answer: '3',student_answer: '1'}];
+var teacher ={id:32,firstname: 'mario',lastname: 'rossi',email: 'prova@gmail.com',user_type: 2,identification_number:123456 };
+var students = [{id:89,firstname: 'francesco',lastname: 'persi',email: 'prova@gmail.com',user_type: 1,identification_number:1875698 },
+                {id:23,firstname: 'paolo',lastname: 'persi',email: 'prova@gmail.com',user_type: 1,identification_number:187546 }];
+
+
+
+
+
 
 //test for get exams list for teacher and student
 test("response 202 for get the examslist", async function () {
@@ -22,8 +27,44 @@ test("validate response of get list ", async function () {
   var exams = response.body;
   expect(response.body).toBeDefined();
 });
-//test for get an exam from list for student and teacher
+test('should return a valid JSON array if user has the permissions to view the users list', async () => {
+            var response = await request(server).get('/v1/exams/').send().set('accept', 'application/json');
+            expect.assertions(11);
+            expect(response.status).toBe(200);
+            expect(response.get('content-type')).toContain('application/json');
+            expect(response.body).not.toBeNull();
+            expect(typeof response.body[0]).toBe('object');
+            expect(response.body[0].id).toBeDefined();
+            expect(response.body[0].description).toBeDefined();
+            expect(response.body[0].deadline).toBeDefined();
+            expect(response.body[0].numerotasks).toBeDefined();
+            expect(response.body[0].teacher).toBeDefined();
+            expect(response.body[0].tasks).toBeDefined();
+            expect(response.body[0].students).toBeDefined();
+  });
 
+
+  test('it should return all the examsList', async () => {
+			var response = await request(server).get('/v1/exams/').set('content-type', 'application/json');
+      var body_keys = ['id','description','deadline','numerotasks','teacher','tasks','students']
+      expect.assertions(15);
+			expect(response).not.toBeNull();
+			expect(response.headers).toBeDefined();
+			expect(response.headers).not.toBeNull();
+			expect(response.status).toBe(200);
+			expect(typeof response.body[0]).toEqual('object');
+			expect(response.body).toBeDefined();
+			expect(response.body).not.toBeNull();
+			expect(typeof response.body[0].id).toEqual('number');
+			expect(typeof response.body[0].description).toEqual('string');
+      expect(response.body[0].deadline).toEqual(3600);
+      expect(response.body[0].numerotasks).toEqual(2);
+      expect(response.body[0].teacher).toEqual(teacher);
+      expect(response.body[0].tasks).toEqual(tasks);
+      expect(response.body[0].students).toEqual(students);
+      expect(Object.keys(response.body[0]).sort()).toEqual(body_keys.sort());
+		});
+//test for get an exam from list for student and teacher
 test("response 202 for get a exam from list", async function () {
   var response = await request(server).get('/v1/exams/2');
   expect(response.status).toBe(200);
