fbbc174323f3fe058e38427b03ec43086cb97074
francesco <francesco.gava@studenti.unitn.it>
2018-12-06 18:05:52 +0100

    updated file exam controller with funtion controler

--- controllers/exams.controller.js
+++ controllers/exams.controller.js
@@ -14,7 +14,7 @@ function getExamsById(id)
 {
   const index = (getExamsList()).findIndex((item)=> {return item.id===id})
   if (index===-1)
-    return 'Not Found'
+  return 'Not Found'
   try {
     return examRepositories.getExamForId(index)
   }
@@ -24,67 +24,57 @@ function getExamsById(id)
 }
 function postExams(exam_post, identity)
 {
-  if((exam_post['teacher'])['id']!=identity.user_id || identity.user_role!= 2)
-  return 'No permission'
-  else {
-        if(exam_post.description === undefined || exam_post.numerotasks=== undefined)
-        return 'Error'
-        idSequence++;
-        exam_post.id = idSequence
-        examList.push(exam_post)
-        examRepositories.updateList(examList)
-        return exam_post
-  }
+  if(exam_post.description === undefined || exam_post.numerotasks=== undefined)
+  return 'Error'
+  idSequence++;
+  exam_post.id = idSequence
+  examList.push(exam_post)
+  examRepositories.updateList(examList)
+  return exam_post
 }
 function putExamsById(id, propertiesChanged, identity) {
-  if(id === undefined || identity === undefined)
-  return 'Error'
   if(parseInt(identity.user_role)!= 2)
-     return 'No permission'
-  if (propertiesChanged === undefined)
-      return 'Not Found'
+  return 'No permission'
   var examFromDB = getExamsById(id);
-
   if(examFromDB=='Not Found')
-        return 'Not Found'
+  return 'Not Found'
   else {
 
-          if(((examFromDB['teacher'])['id'])!= parseInt(identity.user_id ))
-          return 'No permission'
-          else {
-              if(examFromDB === 'Error')
-                return examFromDB
-              else
-              {
+    if(((examFromDB['teacher'])['id'])!= parseInt(identity.user_id ))
+    return 'No permission'
+    else {
+      if(examFromDB === 'Error')
+      return examFromDB
+      else
+      {
 
-                for(let p of propertiesForUpdate) {
-                  let value = propertiesChanged[p];
-                  if(examFromDB[p]!= value){
-                    examFromDB[p] = value;
-                }
-              }
-              deleteExamsById(id,identity)
-              examRepositories.addEx(examFromDB)
-              return examFromDB
-            }
+        for(let p of propertiesForUpdate) {
+          let value = propertiesChanged[p];
+          if(examFromDB[p]!= value){
+            examFromDB[p] = value;
           }
         }
+        deleteExamsById(id,identity)
+        examRepositories.addEx(examFromDB)
+        return examFromDB
+      }
     }
+  }
+}
 function deleteExamsById(id,identity)
 {
 
   if( parseInt(identity.user_role)!= 2)
-    return 'No permission'
+  return 'No permission'
   const index = (getExamsList()).findIndex((item)=> {return item.id===id})
   if(index===-1)
-    return 'Not Found'
+  return 'Not Found'
   else{
-    //console.log(((examRepositories.getExamForId(index))['teacher'])['id'],identity.user_id)
     if(((examRepositories.getExamForId(index))['teacher'])['id']!=parseInt(identity.user_id))
-        return 'No permission'
+    return 'No permission'
     else {
-          examRepositories.deleteEx(index)
-          return getExamsList();
+      examRepositories.deleteEx(index)
+      return getExamsList();
     }
   }
 }
