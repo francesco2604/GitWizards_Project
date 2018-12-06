49657c5c74db7bf0ec83427dbb46c3c9598e91de
Omarechbiki <41294140+Omarechbiki@users.noreply.github.com>
2018-12-06 18:08:55 +0100

    Added STRUCT for Task type
    Added STRUCT for Task type OPEN, MULTIPLE CHOICES OR SINGLE CHOICE

--- models/task.model.js
+++ models/task.model.js
@@ -1,16 +1,24 @@
 'use strict'
+const TASK_TYPE = Object.freeze({
+    OPEN: 0,
+    SINGLE_ANSWER : 1,
+    MULTIPLE_ANSWER : 2
+});
 
 class Task {
     constructor(id, question, type, answers, correct_answer, student_answer){
         this._id = id;
         this._question = question;
-        this._type = type;
-        this._answers = answers;
-        this._correct_answer = correct_answer;
+        this._type = type;// 0=aperta .. 1=risposta singola 2=risposta aperta
+        this._answers = answers;//ARRAY OF ANSWERS
+        this._correct_answer = correct_answer; // only if type is 1 2
         this._student_answer = student_answer;
     }
 
     // GETTERS
+    static get TASK_TYPE(){
+        return TASK_TYPE;
+    }
     get id(){
         return this._id;
     }
@@ -55,7 +63,6 @@ class Task {
         return ({
             id: this.id,
             firstname: this.firstname,
-            type:this.type,
             question: this.question,
             answers: this.answers,
             correct_answer: this.correct_answer,
