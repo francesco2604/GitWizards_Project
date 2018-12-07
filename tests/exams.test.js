27e8052cc032dc1c79e48d08abb62e823dfdfa0f
francesco <francesco.gava@studenti.unitn.it>
2018-12-07 12:00:53 +0100

    #18 updated file exams test

--- tests/exams.test.js
+++ tests/exams.test.js
@@ -90,7 +90,7 @@ test("validate response of get by id  in a exam  list ", async function () {
   expect(response.body.students).toBeDefined();
 });
 
-//test for post an exam
+//TEST FOR POST 
 
 test(" response 403 for post a exam ----no user id ", async function () {
   var response = await request(server).post('/v1/exams/').send(new Exam(66, 'prova3', 3600, 2,teacher, tasks,students)).set('user_id','3').set('user_role','2').set('content-type', 'application/json').set('accept', 'application/json');
