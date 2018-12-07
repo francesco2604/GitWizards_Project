d0d8334b85de6acbe8e0141ebbd6d29aeeb112e0
Alessio Paternoster <AlessioPaternoster@users.noreply.github.com>
2018-12-07 21:20:06 +0100

    Add test cases for DELETE http method #14

--- tests/users.test.js
+++ tests/users.test.js
@@ -27,6 +27,15 @@ function makeGetRequest(server, url, headers, query_params){
     }
     return req.send();
 }
+function makeDeleteRequest(server, url, headers){
+    var req = request(server).delete(url).set('accept', 'application/json');
+    if(headers){
+        headers.forEach((header) => {
+            req = req.set(header);
+        });
+    }
+    return req.send();
+}
 function checkError(response, error){
     var error_obj_keys = ['code', 'message']
     expect.assertions(6);
@@ -180,6 +189,77 @@ describe('## API users', () => {
         test('should return error if user has no permissions to view the list: the type of user has not the right to access to this resource', async() => {
             var response = await makeGetRequest(SERVER, '/v1/users',  bad_headers[1]);
             checkError(response, no_permission_error);
+            return;
+        });
+    });
+    describe('DELETE /v1/users/:id - delete a user from the system', () => {
+        // common
+        var good_user_id = [ 1, 2, 3 ];
+        var bad_user_id = [ 35, undefined, null, 'id_test' ];
+        var no_permission_delete_error =  new Error(Error.ERROR_CODE.FORBIDDEN, 'Accesso negato. Mancanza di permessi per eliminare la risorsa.');
+        var bad_params_error = new Error(Error.ERROR_CODE.BAD_REQUEST, 'Errore nella cancellazione. Parametri non corretti.');
+
+        test('should return a successful deletion message if the user we want to delete exists and we have the permission to do that', async () => {
+            var response = await makeDeleteRequest(SERVER, `/v1/users/${good_user_id[0]}`, [{ user_id : good_user_id[0] }]);
+            expect.assertions(5);
+            expect(response.status).toBe(200);
+            expect(response.get('content-type')).toContain('application/json');
+            expect(typeof response.body.status).toBe('number');
+            expect(typeof response.body.message).toBe('string');
+            expect(response.body).toEqual({
+                status : 200,
+                message : 'Cancellazione effettuata con successo.'
+            });
+        });
+        test('after a successful deletion, server should contain a list of users without the user that was removed', async () => {
+            var response = await makeGetRequest(SERVER, '/v1/users', [{ user_id : 2},{user_role : User.USER_TYPE.TEACHER}]);
+            expect.assertions(5);
+            expect(response.status).toBe(200);
+            expect(response.get('content-type')).toContain('application/json');
+            expect(response.body).not.toBeNull();
+            expect(Array.isArray(response.body)).toBe(true);
+            response.body.forEach((user) => {
+                expect(user.id).not.toBe(good_user_id[0]);
+            });
+        });
+        test('should return an error if the user does not have the permissions to make this request: user is different from the one we want to remove', async () => {
+            var response = await makeDeleteRequest(SERVER, `/v1/users/${good_user_id[1]}`, [{ user_id : good_user_id[2] }]);
+            checkError(response, no_permission_delete_error);
+            return;
+        });
+        test('should return an error if the user does not have the permissions to make this request: user doesn\'t exists', async () => {
+            var response = await makeDeleteRequest(SERVER, `/v1/users/${good_user_id[1]}`, [{ user_id : bad_user_id[0] }]);
+            checkError(response, no_permission_delete_error);
+            return;
+        });
+        test('should return an error if the user does not have the permissions to make this request: user_id is undefined', async () => {
+            var response = await makeDeleteRequest(SERVER, `/v1/users/${good_user_id[1]}`);
+            checkError(response, no_permission_delete_error);
+            return;
+        });
+        test('should return an error if the user does not have the permissions to make this request: user_id is null', async () => {
+            var response = await makeDeleteRequest(SERVER, `/v1/users/${good_user_id[1]}`, [{ user_id : bad_user_id[2] }]);
+            checkError(response, no_permission_delete_error);
+            return;
+        });
+        test('should return an error if the user does not have the permissions to make this request: user_id is alpha string', async () => {
+            var response = await makeDeleteRequest(SERVER, `/v1/users/${good_user_id[1]}`, [{ user_id : bad_user_id[3] }]);
+            checkError(response, no_permission_delete_error);
+            return;
+        });
+        test('should return an error if the user ID specified as URL parameter is wrong: undefined', async () => {
+            var response = await makeDeleteRequest(SERVER, `/v1/users/${bad_user_id[1]}`, [{ user_id : good_user_id[1] }]);
+            checkError(response, bad_params_error);
+            return;
+        });
+        test('should return an error if the user ID specified as URL parameter is wrong: null', async () => {
+            var response = await makeDeleteRequest(SERVER, `/v1/users/${bad_user_id[2]}`, [{ user_id : good_user_id[1] }]);
+            checkError(response, bad_params_error);
+            return;
+        });
+        test('should return an error if the user ID specified as URL parameter is wrong: alpha-string', async () => {
+            var response = await makeDeleteRequest(SERVER, `/v1/users/${bad_user_id[3]}`, [{ user_id : good_user_id[1] }]);
+            checkError(response, bad_params_error);
             return;
         });
     });
