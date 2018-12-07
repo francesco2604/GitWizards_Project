2af7610be6902e5457d8af062ddde47239ba28bd
Sebastian-Fleaca <sebastianioan.fleaca@studenti.unitn.it>
2018-12-07 13:17:05 +0100

    #35 update Submission POST

--- routers/submission.router.js
+++ routers/submission.router.js
@@ -1,9 +1,23 @@
-const express = require('express');
-const SUBMISSION = require('../controllers/submission.controller.js');
+const EXPRESS = require('express');
+const SUBMISSION_CONTROLLER = require('../controllers/submission.controller.js');
 
-const ROUTER = express.Router();
+const ROUTER = EXPRESS.Router();
 
 /* Submission POST */
-ROUTER.post('/', SUBMISSION.postSubmission);
+ROUTER.post('/', (req, res) => {
+    try {
+        if (req.body instanceof Object && Object.keys(req.body).length >= 6) {
+            var submission = SUBMISSION_CONTROLLER.postSubmission(req.body);
+            res.status(201).json(submission);
+        }
+        else {
+            res.status(400).send("Not a submission.\nMake sure you\'re sending a Submission json object.");
+        }
+    }
+    catch (error) {
+        res.status().end();
+    }
+
+});
 
 module.exports = ROUTER;
--- tests/submission.test.js
+++ tests/submission.test.js
@@ -1,24 +1,85 @@
-const request = require('supertest');
-const API = require('../app.js');
+const API = require('../app');
+var request = require('supertest');
 
+/* Example variables */
+var test_task = {
+  numeroDomanda: 2,
+  question: 'diametro della Terra?',
+  type: 1,
+  answers: ['9.742 km',
+    '19.742 km',
+    '12.742 km'],
+  correctAnswer: '3',
+  studentAnswer: '1'
+};
+
+var exampleExam = {
+  example: null,
+  description: 'esame di valutazione conoscenze generali',
+  deadline: 3600,
+  numerotasks: 2,
+  teacher: {
+    id: 32,
+    firstname: 'Marco',
+    lastname: 'Giunta',
+    email: 'marco.giunta@example.com',
+    type: 'Teacher',
+    identification_number: 908765
+  },
+  tasks: [test_task,
+    {
+      id: 456,
+      numeroDomanda: 3,
+      question: 'diametro della Luna?',
+      type: 1,
+      answers: [
+        '4.742 km',
+        '14.742 km',
+        '8.742 km'
+      ],
+      correctAnswer: 3
+    }
+  ],
+  students: [
+    {
+      id: 12,
+      firstname: 'Mario',
+      lastname: 'Rossi',
+      email: 'mario.rossi@example.com',
+      type: 'Student',
+      identification_number: 345678
+    }
+  ]
+};
+
+var exampleError = "123";
+
+/* Test starting */
 beforeAll(() => {
- console.log('API test for Submission starting!');
+  console.log('API test for Submission starting!');
 });
 
 /* Test cases for Submission */
-describe('Tests for PeerReview', () => {
-  
+describe('Tests for Submission', () => {
+
   /* Test POST method in Submission*/
   // Status: 201, Created
-  test('Submissions POST', async () => {
-    let res = await request(API).post('/v1/submissions');
-    expect.assertions(2);
+  test('Submission POST', async () => {
+    let res = await request(API).post('/v1/submission').send(exampleExam);
+    expect.assertions(1);
     expect(res.status).toBe(201);
-    expect(res.get('content-type')).toContain('application/json');
+  });
+
+  // Status: 400, Not an object
+  test('Submission POST', async () => {
+    let res = await request(API).post('/v1/submission').send(exampleError);
+    expect.assertions(1);
+    expect(res.status).toBe(400);
   });
 
 });
 
+/* Test ending */
 afterAll(() => {
   API.close();
 });
